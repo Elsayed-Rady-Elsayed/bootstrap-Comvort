328a902bf0331ab64b40ee9061c7cfb870d7588a
Elsayed-Rady-Elsayed <119011348+Elsayed-Rady-Elsayed@users.noreply.github.com>
2023-06-06 17:55:48 +0300

    Update enc.js

--- enc.js
+++ enc.js
@@ -1,10 +1,9 @@
-console.log("d");
 document.getElementById("menu").onclick = () => {
   if (document.getElementById("sm-menu").style.display == "none") {
-    document.getElementById("sm-menu").style.display = "block";
-
-    console.log("gg");
+    document.getElementById("sm-menu").style.cssText =
+      "display:block !important;";
   } else {
-    document.getElementById("sm-menu").style.display = "none";
+    document.getElementById("sm-menu").style.cssText =
+      "display:none !important";
   }
 };
